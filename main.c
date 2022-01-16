78eaf0ab1acf113e14dcbf3d825524c7e0a323de
John Moore <84421728+johnmooremmc@users.noreply.github.com>
2022-01-17 01:28:20 +1300

    Correction Upload

--- main.c
+++ main.c
@@ -1,60 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-// NOT MY CODE
-
-
-
-
-
-
-
-
-
-void swap(char *x, char *y)
-{
-    char temp;
-    temp = *x;
-    *x = *y;
-    *y = temp;
-}
-
-void permute(char *a, int l, int r)
-{
-int i;
-if (l == r)
-    printf("%s\n", a);
-else
-{
-    for (i = l; i <= r; i++)
-    {
-        swap((a+l), (a+i));
-        permute(a, l+1, r);
-        swap((a+l), (a+i)); //backtrack
-    }
-}
-}
-
-/* Driver program to test above functions */
 int main()
 {
+
     char str[64];
+    char plch[64];
+    char str_holder[64];
+    char stack[256][256];
+
+    int len;
+    int i, j, k;
+    int fac = 1;
+
     printf("Enter A String: ");
     scanf("%s", &str);
-    int n = strlen(str);
-    permute(str, 0, n-1);
+    len = strlen(str);
+
+
+    for(i=0; i<len; i++) {
+        str_holder[i] = str[i];
+        plch[i] = str[i];
+            }
+
+    for(i=1; i<len; i++)
+    {
+        fac *= i+1;
+    }
+
+    printf("\n");
+    printf("%s Has %d Arrangements\n", str, fac);
+    printf("\n");
+
+
+    for(j=0; j<=len-1; j++)
+    {
+        printf("\n------------\n");
+
+        for(i=0; i<len; i++)
+        {
+            //printf("\n%d, %d, %d\n", i, j, len);
+            if (i+j == len-1)
+            {
+                plch[0] = str[0];
+                str[0] = str[i+j];
+                str[i+j] = plch[0];
+
+
+            }
+            else if (i+j < len-1)
+            {
+                plch[i+j] = str[i+j+1];
+                str[i+j+1] = str[i+j];
+                str[i+j] = plch[i+j];
+            } else
+            {
+
+                for (k=0; k<j; k++)
+                {
+                    plch[k] = str[k+1];
+                    str[k+1] = str[k];
+                    str[k] = plch[k];
+                }
+
+            }
+
+                printf("%d : %s\n", i, str);
+
+        }
+        // str = str_holder
+        strcpy(str, str_holder);
+        strcpy(plch, str_holder);
+
+    }
 
 
 
 
     return 0;
 }
-
-
-
-
-
-
-
-
