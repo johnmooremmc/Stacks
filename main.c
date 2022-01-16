329a9f538009aa5c30db17c939607838704c3dbc
John Moore <84421728+johnmooremmc@users.noreply.github.com>
2022-01-17 01:29:36 +1300

    Correction Upload
    lol, how did I miss upload the wrong thing twice...

--- main.c
+++ main.c
@@ -1,81 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+int stack[256];
+int top, insert;
+int i, j, k;
+int rep = 5;
+int pushable = 1;
+
+void Push(int insert)
+{
+    stack[top] = insert;
+    top++;
+}
+
+void Pop()
+{
+    // No popping for now
+    stack[top] = NULL;
+    top--;
+}
+
+void Search(int insert)
+{
+    if (top == 0)
+    {
+        Push(insert);
+    } else
+    {
+        for(j=0; j<top; j++) {
+            if (insert == stack[j])
+            {
+                pushable = 0;
+            }
+        }
+        if (pushable)
+        {
+            Push(insert);
+        } else
+        {
+            pushable = 1;
+        }
+    }
+}
+
 int main()
 {
-
-    char str[64];
-    char plch[64];
-    char str_holder[64];
-    char stack[256][256];
-
-    int len;
-    int i, j, k;
-    int fac = 1;
-
-    printf("Enter A String: ");
-    scanf("%s", &str);
-    len = strlen(str);
-
-
-    for(i=0; i<len; i++) {
-        str_holder[i] = str[i];
-        plch[i] = str[i];
-            }
-
-    for(i=1; i<len; i++)
+    printf("Add %d Values to the stack:\n", rep);
+    for(i=0; i<rep; i++)
     {
-        fac *= i+1;
+        scanf("\n%d\n", &insert);
+        Search(insert);
+        // bug where for loop runs one too long, or the stack is pushed one to little times
     }
 
-    printf("\n");
-    printf("%s Has %d Arrangements\n", str, fac);
-    printf("\n");
-
-
-    for(j=0; j<=len-1; j++)
+    for(k=0; k<top; k++)
     {
-        printf("\n------------\n");
-
-        for(i=0; i<len; i++)
-        {
-            //printf("\n%d, %d, %d\n", i, j, len);
-            if (i+j == len-1)
-            {
-                plch[0] = str[0];
-                str[0] = str[i+j];
-                str[i+j] = plch[0];
-
-
-            }
-            else if (i+j < len-1)
-            {
-                plch[i+j] = str[i+j+1];
-                str[i+j+1] = str[i+j];
-                str[i+j] = plch[i+j];
-            } else
-            {
-
-                for (k=0; k<j; k++)
-                {
-                    plch[k] = str[k+1];
-                    str[k+1] = str[k];
-                    str[k] = plch[k];
-                }
-
-            }
-
-                printf("%d : %s\n", i, str);
-
-        }
-        // str = str_holder
-        strcpy(str, str_holder);
-        strcpy(plch, str_holder);
-
+        printf("    %d", stack[k]);
     }
-
-
-
 
     return 0;
 }
